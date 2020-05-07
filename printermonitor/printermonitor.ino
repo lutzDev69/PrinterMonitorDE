5015a000ded3d232209d8115f652f6a0e9bd0236
lutzDev69 <lutz.hasberg@web.de>
2020-05-07 19:50:07 +0200

    Show cloudiness percentage on web and display

--- printermonitor/printermonitor.ino
+++ printermonitor/printermonitor.ino
@@ -839,11 +839,19 @@ void displayPrinterStatus() {
     } else {
       html += "<div class='w3-cell-row' style='width:100%'><h2>" + weatherClient.getCity(0) + ", " + weatherClient.getTempRounded(0) + getTempSymbol(true) +"</h2></div><div class='w3-cell-row'>";
       html += "<div class='w3-cell w3-left' style='width:300px'>";
-      html += "<img src='http://openweathermap.org/img/wn/" + weatherClient.getIcon(0) + ".png' alt='" + weatherClient.getDescription(0) + "'><span class='w3-large'>" +getTranslate() +"</span><br>";
-      html += weatherClient.getDescription(0) + " (" + weatherClient.getCloudiness(0) + "%)<br>";
-      html += "<span class='w3-medium'>" + weatherClient.getHumidity(0) + "% Luftfeuchtigkeit</span><br>";
-      html += "<span class='w3-medium'>" + weatherClient.getWind(0) + "&nbsp;</span><span class='w3-tiny'>" + getSpeedSymbol() + "</span><span class='w3-medium'> Windstärke</span><br>";
-      html += "<a class='w3-medium' href='https://www.google.com/maps/@" + weatherClient.getLat(0) + "," + weatherClient.getLon(0) + ",10000m/data=' target='_BLANK'><i class='fa fa-map-marker' style='color:red'></i> Karte!</a><br>";
+      html += "<img src='http://openweathermap.org/img/wn/" + weatherClient.getIcon(0) + ".png' alt='" + weatherClient.getDescription(0) + "'><span class='w3-large'>" +getTranslate() +"</span><br />";
+      
+      if (weatherClient.getCloudiness(0).toInt() > 10)
+      {
+        html += weatherClient.getDescription(0) + " (" + weatherClient.getCloudiness(0) + "%)<br />";
+      } else 
+      {
+        html += weatherClient.getDescription(0) + "<br />";
+      }
+      
+      html += "<span class='w3-medium'>" + weatherClient.getHumidity(0) + "% Luftfeuchtigkeit</span><br />";
+      html += "<span class='w3-medium'>" + weatherClient.getWind(0) + "&nbsp;</span><span class='w3-tiny'>" + getSpeedSymbol() + "</span><span class='w3-medium'> Windstärke</span><br />";
+      html += "<a class='w3-medium' href='https://www.google.com/maps/@" + weatherClient.getLat(0) + "," + weatherClient.getLon(0) + ",10000m/data=' target='_BLANK'><i class='fa fa-map-marker' style='color:red'></i> Karte!</a><br />";
       html += "</div>";
       html += "</div>";
     }
